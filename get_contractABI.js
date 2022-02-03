a19af99593b9b6f87ff1566ca5f9018c98a97375
gazon <gseidalinov@gmail.com>
2022-02-03 22:25:34 +0100

    contract ABI address and APIKeyToken changes

--- get_contractABI.js
+++ get_contractABI.js
@@ -1,26 +1,8 @@
 // module
 const https = require('https');
-const axios = require('axios');
 
-const ApiKeyToken = '7DYYBFFH4R4B31ZQMYTB2B2TWIKDWAJB48';
-const address = '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc';
-
-/*
-// USING AXIOS (WILL NOT WORK IN MAIN JS AS IT'S ASYNC)
-async function get_abi(_address, _ApiKeyToken) {
-	var response;
-	try {
-		response = await axios.get('https://api.etherscan.io/api?module=contract&action=getabi&address='+_address+'&apikey='+_ApiKeyToken);
-		console.log("response", JSON.parse(response.data.result));
-	} catch (error) {
-		console.log("error", error);
-	}
-};
-async function run_get_abi(_address, _ApiKeyToken) {
-	await get_abi(_address, _ApiKeyToken);
-}
-run_get_abi(address, ApiKeyToken);
-*/
+const ApiKeyToken = 'YOUR_ETHERSCAN_API_TOKEN';
+const address = 'YOUR_DESIRED_SMART_CONTRACT_ADDRESS';
 
 // USING HTTPS (WILL NOT WORK IN MAIN JS AS IT ALOS APPEARS ASYNC)
 const req = https.request({hostname:'api.etherscan.io',path:'/api?module=contract&action=getabi&address='+address+'&apikey='+ApiKeyToken,method:'GET'}, res => {
